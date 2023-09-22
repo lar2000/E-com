48344d5233c0049f2e84f71ea3457f6e776b2b87
piayang <pbounmayang123@gmail.com>
2023-09-22 21:51:01 +0700

    first_commit

--- userapp/views.py
+++ userapp/views.py
@@ -26,7 +26,6 @@ def register(request):
                 is_staff = True,
                 is_superuser = True
             )
-                
             obj_user.save()
             
             messages.success(request, 'ລົງທະບຽນສຳເລັດແລ້ວ')
